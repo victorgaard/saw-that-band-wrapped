dbc283fb5d9a426af7f4005ee0d9ee241d7958d2
Victor F. Santos <victor.gaard@gmail.com>
2025-01-17 10:01:52 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex h-dvh flex-col items-center justify-center bg-zinc-950 text-2xl font-medium text-zinc-100">
+      Saw that band wrapped 2024 coming
+    </main>
+  );
+}
+
+export default App;
